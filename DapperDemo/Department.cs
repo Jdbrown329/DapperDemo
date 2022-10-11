f5f51abf56aacd0a7cd9863885758490bb5150d0
Jordan Brown <j.d.brown329@gmail.com>
2022-10-11 10:53:52 -0500

    Finished

---
+++ DapperDemo/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDemo
+{
+    public class Department
+    {
+        public Department()
+        {
+
+        }
+        public string Name { get; set; }
+        public string DepartmentID { get; set; }
+    }
+}
--- DapperDemo/Program.cs
+++ DapperDemo/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DapperDemo;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Transactions;
+
+
+using System;
+using System.IO;
+//^^^^MUST HAVE USING DIRECTIVES^^^^
+
+    var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+    string connString = config.GetConnectionString("DefaultConnection");
+    IDbConnection conn = new MySqlConnection(connString);
+    var repo = new DapperDepartmentRepository(conn);
+
+    Console.WriteLine("Type a new Department Name");
+
+        var newDepartment = Console.ReadLine();
+      
+        repo.InsertDepartment(newDepartment);
+
+        var departments = repo.GetAllDepartments();
+
+    foreach (var dept in departments)
+    {
+        Console.WriteLine(dept.Name);
+    }
